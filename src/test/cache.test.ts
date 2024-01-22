1aec18f7922aba602e791e87c9b46f450aac98ab
shambhu_bcoder <shambhunath@bcoder.in>
2024-01-22 18:52:33 +0530

    code fix

--- src/test/cache.test.ts
+++ src/test/cache.test.ts
@@ -1,45 +1,39 @@
-/* global describe, it, before, beforeEach, afterEach */
-import * as chai from 'chai';
-import sinon from 'sinon';
-import Cache from '../index'; // Assuming that your Cache class is exported from the '../index' file
+import Cache from '../index';
 
-chai.use(require('chai-sinon')); // Replace sinonChai with chai-sinon
-const expect = chai.expect;
+let cache: any;
+let clock: any;
+
+beforeEach(() => {
+      clock = jest.spyOn(global, 'setTimeout').mockImplementation((fn) => fn());
+      cache = new Cache();
+});
+
+afterEach(() => {
+      clock.mockRestore();
+});
 
 describe('node-cache', () => {
-      let cache: Cache;
-      let clock: sinon.SinonFakeTimers;
-
-      beforeEach(() => {
-            clock = sinon.useFakeTimers();
-            cache = new Cache();
-      });
-
-      afterEach(() => {
-            clock.restore();
-      });
-
       describe('put()', () => {
-            before(() => {
+            beforeAll(() => {
                   cache.debug(false);
             });
 
             it('should allow adding a new item to the cache', () => {
                   expect(() => {
                         cache.put('key', 'value');
-                  }).to.not.throw();
+                  }).not.toThrow();
             });
 
             it('should allow adding a new item to the cache with a timeout', () => {
                   expect(() => {
                         cache.put('key', 'value', 100);
-                  }).to.not.throw();
+                  }).not.toThrow();
             });
 
             // Other test cases...
 
             it('should return the cached value', () => {
-                  expect(cache.put('key', 'value')).to.equal('value');
+                  expect(cache.put('key', 'value')).toEqual('value');
             });
       });
 
@@ -50,14 +44,11 @@ describe('node-cache', () => {
                   const cache1 = new Cache();
                   const cache2 = new Cache();
                   cache1.put('key', 'value1');
-                  expect(cache1.keys()).to.deep.equal(['key']);
-                  expect(cache2.keys()).to.deep.equal([]);
+                  expect(cache1.keys()).toEqual(['key']);
+                  expect(cache2.keys()).toEqual([]);
                   cache2.put('key', 'value2');
-                  expect(cache1.get('key')).to.equal('value1');
-                  expect(cache2.get('key')).to.equal('value2');
+                  expect(cache1.get('key')).toEqual('value1');
+                  expect(cache2.get('key')).toEqual('value2');
             });
       });
 });
-function before(arg0: () => void) {
-      throw new Error('Function not implemented.');
-}
